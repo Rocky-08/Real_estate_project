6a2fb8ce8b2e578d0d04ea035f8fe615a99c959b
Kartik <kartikgoyal0852@gmail.com>
2023-07-12 22:38:01 +0530

    Add image

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container } from "@material-ui/core";
-//import MainImage from "./MainImage";
+import MainImage from "./MainImage";
 import Heading from "./Heading";
 import AboutPara from "./AboutPara";
 import LiveFacts from "./LiveFacts";
@@ -10,7 +10,7 @@ import Footer from "../Footer/Footer";
 const About = () => {
   return (
     <>
-      {/* <MainImage /> */}
+      <MainImage />
       <Container>
         <Heading text="About Us" align="center" margin="6rem" font="light" />
         <AboutPara />
@@ -19,7 +19,7 @@ const About = () => {
       <Container style={{ marginBottom: "4rem" }}>
         <Members />
       </Container>
-      
+
       <Container style={{ backgroundColor: "#f1f1f1", paddingTop: "3rem" }}>
         <Heading text="Live Facts" align="left" margin="2rem" font="bold" />
         <LiveFacts />
--- src/About/MainImage.jsx
+++ src/About/MainImage.jsx
@@ -3,15 +3,11 @@ import React from "react";
 const MainImage = () => {
   return (
     <>
-      <div
-        style={{
-          height: "100vh",
-          width: "100%",
-          backgroundImage: `url("https://news.airbnb.com/wp-content/uploads/sites/4/2022/09/Newsroom-About.jpg?w=2048")`,
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-        }}
-      ></div>
+      <img
+        src="https://news.airbnb.com/wp-content/uploads/sites/4/2022/09/Newsroom-About.jpg?w=2048"
+        alt="image"
+        style={{ width: "100%", height: "100%" }}
+      />
     </>
   );
 };
